96ada7c9c3d42a6fb44eac80df80888d6038e7d7
platinum-otabek <mr.tursinboev@gmail.com>
2021-08-26 16:47:04 +0500

    second

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(0fyfe-3xi^@vj*u2256v%v8xpkb4)74^p023&%sxt@)1wfsdk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
